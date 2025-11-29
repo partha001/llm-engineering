cdf0d962f7735a915fe655919934fed902f1836f
Partha Biswas <biswas.partha1@gmail.com>
2025-11-29 16:37:25 +0530

    langgraph program1

---
+++ learning-langgraph/001_workflowBasic/003_langgraphDemo.py
@@ -0,0 +1,35 @@
+from typing import TypedDict
+from langgraph.graph import END, START, StateGraph
+from util.langgraph_util import  display
+
+# defining the state
+class HelloWorldState(TypedDict):
+    message:str
+
+def node1Hello(state: HelloWorldState):
+    print(f"Hello Node: {state['message']}")
+    return {"message": "Hello "+state['message']}
+
+def node2Bye(state: HelloWorldState):
+    print(f"Bye Node: {state['message']}")
+    return {"message": "Bye "+state['message']}
+
+#creating the graph
+graph = StateGraph(HelloWorldState)
+
+#creating the nodes
+graph.add_node("node1", node1Hello)
+graph.add_node("node2", node2Bye)
+
+#creating edges
+#graph.add_edge(START, "node1")
+graph.add_edge("node1", "node2")
+graph.add_edge("node2", END)
+
+graph.set_entry_point("node1")
+
+runnable = graph.compile()
+display(runnable)
+output = runnable.invoke({"message":"partha"})
+print(output)
+
