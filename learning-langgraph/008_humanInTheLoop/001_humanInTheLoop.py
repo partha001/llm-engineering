eecec9c1647dfcf67215c0f72bd956ab7e6ca05f
Partha Biswas <biswas.partha1@gmail.com>
2025-12-09 00:50:30 +0530

    human in the loop

--- learning-langgraph/008_humanInTheLoop/001_humanInTheLoop.py
+++ learning-langgraph/008_humanInTheLoop/001_humanInTheLoop.py
@@ -37,7 +37,7 @@ def generate_code(state):
 def human_review(state):
     print(f"before interrupt. state: {state}\n")
     value = interrupt({})
-    print(f"after interrupt. state: {state}\n")
+    print(f"after interrupt.\n")
     if value == "yes":
         return Command(goto="create_tests")
     else:
